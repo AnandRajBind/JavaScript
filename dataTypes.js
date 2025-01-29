ef7d8e7abbe07fcd4131eb260d17c00cfbf74cd5
Anand Raj Bind <116335833+AnandRajBind@users.noreply.github.com>
2025-01-29 19:01:08 +0530

    types of data type

--- dataTypes.js
+++ dataTypes.js
@@ -1,1 +1,43 @@
 // dataTypes 
+//primitives 
+// 7 types: String , Number, Boolean, null,undefined, Symbol, BigInt.
+
+const score=100;
+const scoreValue=100.3;
+const isLoggedIn=false;
+const outsideTemp=null;
+let userEmail;
+const id=Symbol('123');
+const anotherId=Symbol('123');
+console.log(id === anotherId);
+// const bigNumber=94842424485785454n;
+
+// Reference (non primitives)
+// Array,objects, Functions
+// Note:- all non primitives data types ka data type function hoga.aur function ka return type function object hoga.
+
+
+const heros=["varun Dhawan" , "Tiger","Akshya Kumar"]; 
+
+let myObj={
+    name:"Anand",
+    age: 22,
+}
+
+const myfunction=function(){
+    console.log("My first function in Java");
+}
+// console.log(bigNumber);
+console.log(typeof score);// data types:- number 
+console.log(typeof scoreValue);// data types:- number 
+console.log(typeof isLoggedIn);// data types:- boolean
+console.log(typeof outsideTemp);// data types:- object (null ka data types object hota hai)
+console.log(typeof userEmail);// data types:- undefined (undefined ka data types undefined hota hai)
+console.log(typeof id);//  data types:- symbol (symbol ka data types symbol hota hai)
+ console.log(typeof BigInt);// data types:- function (BigInt ka data types object hota hai)
+console.log(typeof heros);// data types:- object (array ka data types object hota hai)
+console.log(typeof myObj);//  data types:- object (object ka data types object hota hai)
+ console.log(typeof myfunction);//  data types:- function (function ka data types function hota hai)
+ 
+
+ 
