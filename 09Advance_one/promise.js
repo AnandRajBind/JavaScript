57a06b3abc63a13e7a9c24b7659a6cf71edf9fd9
Anand Raj Bind <116335833+AnandRajBind@users.noreply.github.com>
2025-02-28 19:45:59 +0530

    28/02/2025
    this and new keyword

--- 09Advance_one/promise.js
+++ 09Advance_one/promise.js
@@ -129,6 +129,5 @@ fetch("https://jsonplaceholder.typicode.com/users").then(function(response){
     return response.json();
 }).then(function(data){
     console.log(data);
-    
 }).catch((error)=>console.log(error));
 
